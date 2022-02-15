326f7c7fc0702dacc379c0a900690d15568fc996
aymenbs22 <aymenbenslima1@gmail.com>
2022-02-15 13:48:58 -0800

    header file

--- sort.h
+++ sort.h
@@ -20,4 +20,5 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void swap(listint_t **list, listint_t *f, listint_t *s);
 #endif
